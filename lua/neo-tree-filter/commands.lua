c2b8a207c92e1235e5204452839da1e31e3abf6a
Alex <sashazjukov@gmail.com>
2026-02-26 22:15:20 +0300

    new fix

--- lua/neo-tree-filter/commands.lua
+++ lua/neo-tree-filter/commands.lua
@@ -1,9 +1,14 @@
 local cc = require("neo-tree.sources.common.commands")
 local manager = require("neo-tree.sources.manager")
+local source = require("neo-tree-filter.init")
 
 local vim = vim
 
 local M = {}
+
+M.open_filter = function(state)
+  source.open_filter_input(state)
+end
 
 M.refresh = function(state)
   manager.refresh("neo-tree-filter", state)
--- lua/neo-tree-filter/init.lua
+++ lua/neo-tree-filter/init.lua
@@ -10,6 +10,11 @@ local M = {
 }
 
 local filter = require("neo-tree-filter.filter")
+local floating_input = require("neo-tree-filter.floating-input")
+
+local function refresh_source()
+  manager.refresh("neo-tree-filter")
+end
 
 M.navigate = function(state, path)
   if path == nil then
@@ -26,10 +31,13 @@ M.navigate = function(state, path)
     items = {
       {
         id = "placeholder",
-        name = "No filter - open input to type regex",
+        name = "Press Enter to type regex filter",
         type = "message",
       }
     }
+    vim.defer_fn(function()
+      M.open_filter_input(state)
+    end, 100)
   elseif filter_type == "filename" then
     items = filter.filter_by_filename(path, filter_pattern)
   else
@@ -39,8 +47,20 @@ M.navigate = function(state, path)
   renderer.show_nodes(items, state)
 end
 
+M.open_filter_input = function(state)
+  floating_input.open({
+    default_text = state.filter_pattern or "",
+    on_submit = function(value)
+      state.filter_pattern = value
+      state.filter_type = "filename"
+      M.navigate(state)
+    end,
+    on_close = function()
+    end,
+  })
+end
+
 M.setup = function(config, global_config)
-  vim.notify("neo-tree-filter setup called", vim.log.levels.DEBUG)
 end
 
 return M
