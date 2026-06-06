f6e1af18dbabd21a9890cbbfa79e2904971622f9
Alex <sashazjukov@gmail.com>
2026-06-06 16:55:29 +0300

    recolor pattern in wrapper root node

--- lua/neo-tree-filter/components.lua
+++ lua/neo-tree-filter/components.lua
@@ -45,6 +45,15 @@ M.name = function(config, node, state)
 	if node:get_depth() == 1 then
 		highlight = highlights.ROOT_NAME
 	end
+	if node.id and node.id:find("^__filter__:") then
+		local prefix_end = node.name:find(": ")
+		if prefix_end then
+			return {
+				{ text = node.name:sub(1, prefix_end), highlight = highlight },
+				{ text = node.name:sub(prefix_end + 2), highlight = highlights.FILTER_TERM },
+			}
+		end
+	end
 	local count_text = node.name:match("%s+%(%d+%)$")
 	if count_text then
 		local name_part = node.name:sub(1, -(#count_text + 1))
