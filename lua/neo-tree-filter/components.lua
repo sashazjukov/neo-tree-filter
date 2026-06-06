1148d05de7eb1e598da5ffc2718a4cdd6366bccb
Alex <sashazjukov@gmail.com>
2026-06-06 12:01:25 +0300

    highlight filter pattern on root node with NeoTreeFilterTerm

--- lua/neo-tree-filter/components.lua
+++ lua/neo-tree-filter/components.lua
@@ -4,59 +4,60 @@ local common = require("neo-tree.sources.common.components")
 local M = {}
 
 M.custom = function(config, node, state)
-  local text = node.extra.custom_text or ""
-  local highlight = highlights.DIM_TEXT
-  return {
-    text = text .. " ",
-    highlight = highlight,
-  }
+	local text = node.extra.custom_text or ""
+	local highlight = highlights.DIM_TEXT
+	return {
+		text = text .. " ",
+		highlight = highlight,
+	}
 end
 
 M.icon = function(config, node, state)
-  local icon = config.default or " "
-  local padding = config.padding or " "
-  local highlight = config.highlight or highlights.FILE_ICON
-  if node.type == "directory" then
-    highlight = highlights.DIRECTORY_ICON
-    if node:is_expanded() then
-      icon = config.folder_open or "-"
-    else
-      icon = config.folder_closed or "+"
-    end
-  elseif node.type == "file" then
-    local success, web_devicons = pcall(require, "nvim-web-devicons")
-    if success then
-      local devicon, hl = web_devicons.get_icon(node.name, node.ext)
-      icon = devicon or icon
-      highlight = hl or highlight
-    end
-  end
-  return {
-    text = icon .. padding,
-    highlight = highlight,
-  }
+	local icon = config.default or " "
+	local padding = config.padding or " "
+	local highlight = config.highlight or highlights.FILE_ICON
+	if node.type == "directory" then
+		highlight = highlights.DIRECTORY_ICON
+		if node:is_expanded() then
+			icon = config.folder_open or "-"
+		else
+			icon = config.folder_closed or "+"
+		end
+	elseif node.type == "file" then
+		local success, web_devicons = pcall(require, "nvim-web-devicons")
+		if success then
+			local devicon, hl = web_devicons.get_icon(node.name, node.ext)
+			icon = devicon or icon
+			highlight = hl or highlight
+		end
+	end
+	return {
+		text = icon .. padding,
+		highlight = highlight,
+	}
 end
 
 M.name = function(config, node, state)
-  local highlight = config.highlight or highlights.FILE_NAME
-  if node.type == "directory" then
-    highlight = highlights.DIRECTORY_NAME
-  end
-  if node:get_depth() == 1 then
-    highlight = highlights.ROOT_NAME
-    local pattern = state.filter_pattern or ""
-      if pattern ~= "" then
-        local prefix = state.filter_type == "content" and "c" or "f"
-        return {
-          text = node.name .. "  (" .. prefix .. ": " .. pattern .. ")",
-          highlight = highlight,
-        }
-      end
-  end
-  return {
-    text = node.name,
-    highlight = highlight,
-  }
+	local highlight = config.highlight or highlights.FILE_NAME
+	if node.type == "directory" then
+		highlight = highlights.DIRECTORY_NAME
+	end
+	if node:get_depth() == 1 then
+		highlight = highlights.ROOT_NAME
+		local pattern = state.filter_pattern or ""
+		if pattern ~= "" then
+			local prefix = state.filter_type == "content" and "c" or "f"
+			return {
+				{ text = node.name .. "  (", highlight = highlight },
+				{ text = prefix .. ": " .. pattern, highlight = highlights.FILTER_TERM },
+				{ text = ")", highlight = highlight },
+			}
+		end
+	end
+	return {
+		text = node.name,
+		highlight = highlight,
+	}
 end
 
 return vim.tbl_deep_extend("force", common, M)
